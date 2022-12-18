4d5e0381928462944ade94cc1204fcdf5b52ce9f
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2022-12-18 07:55:54 +0100

    set the number of squares, end of this project

--- etch_a_sketch/app.js
+++ etch_a_sketch/app.js
@@ -1,16 +1,17 @@
 "use strict";
+
 let container = document.querySelector("#container");
 
 let setSquares_btn = document.querySelector("#set_squares_btn");
 
 setSquares_btn.addEventListener("click", function () {
-  setSquares();
+  let numberOfSquares = setSquares();
+  let grid_container = document.querySelector("#grid_container");
+  container.removeChild(grid_container);
+  initGrid(numberOfSquares);
 });
 
-initGrid();
-
 function setSquares() {
-  console.log("ok");
   let newNumberSquares = parseInt(prompt("How many squares do you want ?"));
   if (newNumberSquares > 100) {
     newNumberSquares = 100;
@@ -19,20 +20,29 @@ function setSquares() {
   } else if (isNaN(newNumberSquares)) {
     newNumberSquares = 16;
   }
-  console.log(newNumberSquares);
+  return newNumberSquares;
 }
 
-function initGrid() {
+function initGrid(numberOfSquares) {
   let grid_container = document.createElement("div");
   grid_container.classList.add("grid");
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+  grid_container.setAttribute("id", "grid_container");
+
+  let squareLength = 500 / numberOfSquares - 2;
+
+  grid_container.style.gridTemplateColumns = `repeat(${numberOfSquares}, ${squareLength}px)`;
+  grid_container.style.gridTemplateRows = `repeat(${numberOfSquares}, ${squareLength}px)`;
+
+  for (let i = 0; i < numberOfSquares; i++) {
+    for (let j = 0; j < numberOfSquares; j++) {
       let square = document.createElement("div");
       square.classList.add("square");
       grid_container.appendChild(square);
     }
   }
+
   container.appendChild(grid_container);
+
   let squaresNodeList = document.querySelectorAll(".square");
   let squares = Array.from(squaresNodeList);
   for (const square of squares) {
@@ -41,9 +51,9 @@ function initGrid() {
       let randomNumber2 = Math.floor(Math.random() * 300);
       let randomNumber3 = Math.floor(Math.random() * 300);
 
-      console.log(randomNumber1);
-
       square.style.background = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
     });
   }
 }
+
+initGrid(20);
