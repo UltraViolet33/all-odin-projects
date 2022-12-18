0456e0679404b39a3a5662e57ade62a15947b8a8
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2022-12-18 07:24:46 +0100

    delete app.ts

--- etch_a_sketch/app.js
+++ etch_a_sketch/app.js
@@ -1,14 +1,28 @@
-var container = document.querySelector("#container");
+"use strict";
+let container = document.querySelector("#container");
 initGrid();
 function initGrid() {
-    var grid_container = document.createElement("div");
-    grid_container.classList.add("grid");
-    for (var i = 0; i < 16; i++) {
-        for (var j = 0; j < 16; j++) {
-            var square = document.createElement("div");
-            square.classList.add("square");
-            grid_container.appendChild(square);
-        }
+  let grid_container = document.createElement("div");
+  grid_container.classList.add("grid");
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      grid_container.appendChild(square);
     }
-    container.appendChild(grid_container);
+  }
+  container.appendChild(grid_container);
+  let squaresNodeList = document.querySelectorAll(".square");
+  let squares = Array.from(squaresNodeList);
+  for (const square of squares) {
+    square.addEventListener("mouseover", function () {
+      let randomNumber1 = Math.floor(Math.random() * 300);
+      let randomNumber2 = Math.floor(Math.random() * 300);
+      let randomNumber3 = Math.floor(Math.random() * 300);
+
+      console.log(randomNumber1);
+
+      square.style.background = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
+    });
+  }
 }
